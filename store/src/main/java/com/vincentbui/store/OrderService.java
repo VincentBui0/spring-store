d3cd04e74bace68c4a6389b69be218bba323f871
Vincent Bui <vincent.bui657@gmail.com>
2025-06-20 22:16:43 -0400

    Practice setter injection, Spring I oC Container and Configuring Beans using Annotati ons

--- store/src/main/java/com/vincentbui/store/OrderService.java
+++ store/src/main/java/com/vincentbui/store/OrderService.java
@@ -1,13 +1,26 @@
 package com.vincentbui.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
+    public OrderService(PaymentService paymentService, int x) {
+        // Default constructor
+    }
+    @Autowired
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     public void placeOrder() {
         paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
